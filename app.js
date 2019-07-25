629dfeb41754b4bba93251c2b9787aecaa0fa9e3
ANRoebuck <ANRoebuck@googlemail.com>
2019-07-25 12:36:11 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require ('express');
+const webPush = require ('web-push');
+const app = express();
+
+const port = 9090;
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port);
